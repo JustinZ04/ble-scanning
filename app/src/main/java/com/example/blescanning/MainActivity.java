464249f47e26baf64378147e313a6339975f972f
Justin <jzabel04@knights.ucf.edu>
2018-10-25 17:44:57 -0400

    added volley dependency and minor changes

--- app/src/main/java/com/example/blescanning/MainActivity.java
+++ app/src/main/java/com/example/blescanning/MainActivity.java
@@ -82,6 +82,12 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    public void doLogin(View view)
+    {
+        Intent loginIntent = new Intent(this, SignUp.class);
+        startActivity(loginIntent);
+    }
+
     public void sendMessage(View view)
     {
         BluetoothManager mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
