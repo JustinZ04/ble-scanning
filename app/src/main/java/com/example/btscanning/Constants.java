012b31f32b795da3e88366aaf0791340d5948461
matthew <matthew.villegas@knights.ucf.edu>
2018-11-11 19:38:22 -0500

    Added comment reminding to have local apikey

--- app/src/main/java/com/example/btscanning/Constants.java
+++ app/src/main/java/com/example/btscanning/Constants.java
@@ -7,7 +7,7 @@ public class Constants
     public static final ParcelUuid UUID = ParcelUuid
             .fromString("cac426a3-344f-45c8-8819-ebcfe81e4b23");
     public static final String URL = "https://ble-attendance.herokuapp.com";
-    public static final String LOGIN = "/api/professors/login/" + APIKeys.apiKey;
+    public static final String LOGIN = "/api/professors/login/" + APIKeys.apiKey; // Make sure to have local class file
     public static final String Classes = "/api/classes/";
     public static boolean LOGGED_IN = false;
 }
